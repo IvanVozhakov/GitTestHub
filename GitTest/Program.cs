68791e657bfb954a155b7e2101120d66912c5951
User <User@LAPTOP-5B8K7EQ7>
2025-07-17 18:57:49 +0700

    Создал два класса  и изменил мейн

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // создал юзера
+            Console.WriteLine("Hello, Stepik!");
+            // создал человека
         }
     }
 }
---
+++ GitTest/User1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class User1
+    {
+        public int Age = 10;
+        private int Id = 1;
+
+    }
+}
