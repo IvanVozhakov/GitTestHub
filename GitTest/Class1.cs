585adde3180fbc506087a62c643f6b9dd77ae920
User <User@LAPTOP-5B8K7EQ7>
2025-07-17 19:12:05 +0700

    Добавил класс с методом, изменил Main, создал новую папку

---
+++ GitTest/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    public class Class1
+    {
+        public void ABC()
+        { } 
+    }
+}
